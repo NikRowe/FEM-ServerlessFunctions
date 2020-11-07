de61c6c98fb20483ace2a2d51c97be9c55d5c125
Nik Rowe <nikrowedev@gmail.com>
2020-11-07 14:07:52 -0800

    Comments for how to access endpoint

--- functions/boop.js
+++ functions/boop.js
@@ -1,4 +1,6 @@
 /* async used so we don't have to use a CallBack we can just return something --- the serverless f() needs to know how to handle incoming requests which it does that by calling a f() called handler, so we are customizing the handler */
+
+/* You can access this during testing by vising localHost:xxxx/.netlify/functions/<function name here> */
 exports.handler = async () => {
     return {
         statusCode: 200,
